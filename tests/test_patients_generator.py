7e41c563f5fff52ad3ff480aba225f8c4a9cd3c6
Colin Wren <colin@neovahealth.co.uk>
2016-01-06 18:27:04 +0000

    Adding patients generator tests

--- tests/test_patients_generator.py
+++ tests/test_patients_generator.py
@@ -16,3 +16,51 @@ class TestPatientsGenerator(unittest.TestCase):
                                'K', 'L', 'M', 'N', 'P', 'R', 'S', 'Z']
         self.assertEqual(ethnicity_list, patientgen.ethnicity_list,
                          'Ethnicity list incorrect')
+
+    def test_no_update_on_data_element(self):
+        """
+        Make sure that the data element in the output has the noupdate flag
+        set to True
+        """
+        patientgen = PatientsGenerator(0, 0, 0, 'a')
+        no_update = patientgen.data.attrib['noupdate']
+        self.assertEqual(no_update, '1', 'Incorrect noupdate flag')
+
+    def test_has_gender_list(self):
+        """
+        Make sure that the PatientGenerator class has a gender list
+        """
+        patientgen = PatientsGenerator(0, 0, 0, 'a')
+        gender_list = ['M', 'F']
+        self.assertEqual(gender_list, patientgen.gender_sex_list,
+                         'Incorrect Gender Sex List')
+
+    def test_creates_number_of_defined_in_bed_records(self):
+        """
+        Make sure that it generates the number of defined records for patients
+        in bed
+        """
+        patientgen = PatientsGenerator(0, 4, 0, 'a')
+        records = patientgen.data.findall('record')
+        self.assertEqual(4, len(records),
+                         'Incorrect number of records generated')
+
+    def test_creates_number_of_defined_not_in_bed_records(self):
+        """
+        Make sure that it generates the number of defined records for patients
+        not in bed
+        """
+        patientgen = PatientsGenerator(0, 0, 4, 'a')
+        records = patientgen.data.findall('record')
+        self.assertEqual(4, len(records),
+                         'Incorrect number of records generated')
+
+    def test_creates_number_of_defined_records(self):
+        """
+        Make sure that it generates the number of defined records for patients
+        in bed
+        """
+        patientgen = PatientsGenerator(0, 4, 4, 'a')
+        records = patientgen.data.findall('record')
+        self.assertEqual(8, len(records),
+                         'Incorrect number of records generated')

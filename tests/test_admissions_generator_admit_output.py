111a2b63cb825966b22f608bb76b3cffd3bc0af9
Colin Wren <colin@neovahealth.co.uk>
2016-01-06 21:21:47 +0000

    Adding admissions generator admit data output test

--- tests/test_admissions_generator_admit_output.py
+++ tests/test_admissions_generator_admit_output.py
@@ -4,9 +4,9 @@ from demo_data_generators.patients import PatientsGenerator
 from demo_data_generators.admissions import AdmissionsGenerator
 
 
-class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
+class TestAdmissionsGeneratorAdmitOutput(unittest.TestCase):
     """
-    Test that the admissions generator generates ADT admissions properly
+    Test that the admissions generator generates admissions properly
     """
 
     def setUp(self):
@@ -16,9 +16,9 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         self.patientgen = PatientsGenerator(0, 1, 0, 'a')
         self.gen = AdmissionsGenerator(self.patientgen)
         records = self.gen.data.findall('record')
-        self.activity_record = records[0]
-        self.model_record = records[1]
-        self.activity_update_record = records[2]
+        self.activity_record = records[3]
+        self.model_record = records[4]
+        self.activity_update_record = records[5]
         eval_regex = r"(\(datetime\.now\(\) \+ timedelta\(-\d\)\)" \
                      r"\.strftime\('%Y-%m-%d 00:00:00'\))"
         self.eval_regex = re.compile(eval_regex)
@@ -28,7 +28,7 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         Make sure the record has teh correct id and model
         """
         self.assertEqual(self.activity_record.attrib['id'],
-                         'nhc_activity_demo_adt_admit_0',
+                         'nhc_activity_demo_admission_0',
                          'Incorrect ID ')
         self.assertEqual(self.activity_record.attrib['model'],
                          'nh.activity',
@@ -42,6 +42,14 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         self.assertEqual(field.attrib['ref'], 'nhc_demo_patient_0',
                          'Incorrect patient_id on activity')
 
+    def test_activity_creator_id_field(self):
+        """
+        Make sure the creator id field for the  activity is correct
+        """
+        field = self.activity_record.find('field[@name=\'creator_id\']')
+        self.assertEqual(field.attrib['ref'], 'nhc_activity_demo_adt_admit_0',
+                         'Incorrect creator_id on activity')
+
     def test_activity_parent_id_field(self):
         """
         Make sure the parent id field for the  activity is correct
@@ -49,6 +57,14 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         field = self.activity_record.find('field[@name=\'parent_id\']')
         self.assertEqual(field.attrib['ref'], 'nhc_activity_demo_spell_0',
                          'Incorrect parent_id on activity')
+
+    def test_activity_spell_activity_id_field(self):
+        """
+        Make sure the spell activity id field for the  activity is correct
+        """
+        field = self.activity_record.find('field[@name=\'spell_activity_id\']')
+        self.assertEqual(field.attrib['ref'], 'nhc_activity_demo_spell_0',
+                         'Incorrect spell_activity_id on activity')
 
     def test_activity_state_field(self):
         """
@@ -63,7 +79,7 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         Make sure the data model field for the  activity is correct
         """
         field = self.activity_record.find('field[@name=\'data_model\']')
-        self.assertEqual(field.text, 'nh.clinical.adt.patient.admit',
+        self.assertEqual(field.text, 'nh.clinical.patient.admission',
                          'Incorrect data_model on activity')
 
     def test_activity_date_terminated_field(self):
@@ -81,10 +97,10 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         Make sure the record has teh correct id and model
         """
         self.assertEqual(self.model_record.attrib['id'],
-                         'nhc_demo_adt_admit_0',
+                         'nhc_demo_admission_0',
                          'Incorrect ID ')
         self.assertEqual(self.model_record.attrib['model'],
-                         'nh.clinical.adt.patient.admit',
+                         'nh.clinical.patient.admission',
                          'Incorrect model')
     
     def test_activity_id_field(self):
@@ -92,7 +108,7 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         Make sure the activity_id field for the  is correct
         """
         field = self.model_record.find('field[@name=\'activity_id\']')
-        self.assertEqual(field.attrib['ref'], 'nhc_activity_demo_adt_admit_0',
+        self.assertEqual(field.attrib['ref'], 'nhc_activity_demo_admission_0',
                          'Incorrect activity_id on activity')
     
     def test_patient_id_field(self):
@@ -118,14 +134,6 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         field = self.model_record.find('field[@name=\'pos_id\']')
         self.assertEqual(field.attrib['ref'], 'nhc_def_conf_pos_hospital',
                          'Incorrect pos_id on activity')
-
-    def test_location_field(self):
-        """
-        Make sure the locaiton field for the  is correct
-        """
-        field = self.model_record.find('field[@name=\'location\']')
-        self.assertEqual(field.text, 'A',
-                         'Incorrect location on activity')
     
     def test_code_field(self):
         """
@@ -143,29 +151,13 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         regex_match = re.match(self.eval_regex, field.attrib['eval'])
         self.assertEqual(len(regex_match.groups()), 1,
                          'Incorrect start_date eval on activity')
-        
-    def test_patient_identifier_field(self):
-        """
-        Make sure the code field for the  is correct
-        """
-        field = self.model_record.find('field[@name=\'patient_identifier\']')
-        self.assertEqual(field.text, 'NHSNUM0000',
-                         'Incorrect patient identifier on activity')
-        
-    def test_other_identifier_field(self):
-        """
-        Make sure the code field for the  is correct
-        """
-        field = self.model_record.find('field[@name=\'other_identifier\']')
-        self.assertEqual(field.text, 'HOSNUM0000',
-                         'Incorrect other identifier on activity')
     
     def test_activity_update_record(self):
         """
         Make sure the record has teh correct id and model
         """
         self.assertEqual(self.activity_update_record.attrib['id'],
-                         'nhc_activity_demo_adt_admit_0',
+                         'nhc_activity_demo_admission_0',
                          'Incorrect ID ')
         self.assertEqual(self.activity_update_record.attrib['model'],
                          'nh.activity',
@@ -178,7 +170,7 @@ class TestAdmissionsGeneratorADTAdmitOutput(unittest.TestCase):
         field = self.activity_update_record.find('field[@name=\'data_ref\']')
         self.assertEqual(
             field.attrib['eval'],
-            '\'nh.clinical.adt.patient.admit,\' + '
-            'str(ref(\'nhc_demo_adt_admit_0\'))',
+            '\'nh.clinical.patient.admission,\' + '
+            'str(ref(\'nhc_demo_admission_0\'))',
             'Incorrect data ref on activity update'
         )

93e0daedc4066e2243cea3a3636ce91170504b46
Will Earp <will.earp@icloud.com>
2016-01-08 15:26:36 +0000

    [WI-1880] Added Observations and tests

---
+++ demo_setup_tools/observations.py
@@ -0,0 +1,32 @@
+from demo_setup_tools.client import get_erppeek_client
+
+
+class SubmitObservations(object):
+
+    def __int__(self, server, database, user, password, days):
+        client = get_erppeek_client(server=server, db=database, user=user,
+                                    password=password)
+        SubmitInitialObservations(client, days)
+        SubmitFinalObservations(client, days)
+
+
+class SubmitInitialObservations(object):
+
+    def __init__(self, client, days):
+        self.client = client
+        self.submit_observations(days)
+
+    def submit_observations(self, days):
+        api_demo = self.client.model('nh.eobs.demo.loader')
+        api_demo.completed_first_ews_for_placed_patients(days)
+
+
+class SubmitFinalObservations(object):
+
+    def __init__(self, client, days):
+        self.client = client
+        self.submit_observations(days)
+
+    def submit_observations(self, days):
+        api_demo = self.client.model('nh.eobs.demo.loader')
+        api_demo.generate_news_simulation(days)

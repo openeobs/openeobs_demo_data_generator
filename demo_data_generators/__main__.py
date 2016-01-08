c8324dc5343e6942695e982ce6e82a72a24f3383
Colin Wren <colin@neovahealth.co.uk>
2016-01-08 12:55:43 +0000

    Adding CLI for demo data generator

---
+++ demo_data_generators/__main__.py
@@ -0,0 +1,73 @@
+""" CLI foo for demo data generator
+"""
+import sys
+import os
+import argparse
+import json
+from demo_data_generators.demo_data_coordinator import DemoDataCoordinator
+
+DEFAULT_USERS = '{"doctor": {"unassigned": 4, "total": 24, "per_ward": 4}, ' \
+                '"admin": {"unassigned": 0, "multi_wards": "all", "total": 1' \
+                ', "per_ward": 0}, "kiosk": {"unassigned": 0, "total": 5, ' \
+                '"per_ward": 1}, "senior_manager": {"unassigned": 0, ' \
+                '"multi_wards": [["a", "b", "c"], ["d", "e"], ' \
+                '["a", "b", "c", "d", "e"]], "total": 3, "per_ward": 0}, ' \
+                '"hca": {"unassigned": 5, "total": 55, "per_ward": 10}, ' \
+                '"nurse": {"unassigned": 5, "total": 55, "per_ward": 10},' \
+                ' "ward_manager": {"unassigned": 1, "total": 6, "per_ward": ' \
+                '1}}'
+
+PARSER = argparse.ArgumentParser('Generate demo data for NH Clinical')
+PARSER.add_argument('data_folder', type=str,
+                    help='Folder in which to generate the data')
+PARSER.add_argument('--wards', type=str,
+                    help='CSV list of ward names',
+                    default='a,b,c,d,e')
+PARSER.add_argument('--beds', type=int,
+                    help='Number of beds per ward',
+                    default=30)
+PARSER.add_argument('--patientsinbed', type=int,
+                    help='Number of patients in a bed', default=28)
+PARSER.add_argument('--patientsnotinbed', type=int,
+                    help='Number of patients not in a bed', default=12)
+PARSER.add_argument('--users', type=str,
+                    help='JSON for user break down', default=DEFAULT_USERS)
+
+
+def main():
+    """
+    Parse the args and generate the demo data
+    """
+    args = PARSER.parse_args()
+    data_folder = args.data_folder
+    wards = args.wards
+    beds_per_ward = args.beds
+    bed_patient_per_ward = args.patientsinbed
+    non_bed_patient_per_ward = args.patientsnotinbed
+    users_schema = args.users
+
+    if wards:
+        wards = wards.replace(' ', '').split(',')
+    if users_schema:
+        users_schema = json.loads(users_schema)
+    if data_folder:
+        data_folder = sanitise_data_folder(data_folder)
+
+    DemoDataCoordinator(wards=wards,
+                        beds_per_ward=beds_per_ward,
+                        bed_patient_per_ward=bed_patient_per_ward,
+                        non_bed_patient_per_ward=non_bed_patient_per_ward,
+                        users_schema=users_schema,
+                        data_folder=data_folder)
+
+
+def sanitise_data_folder(folder_path):
+    if '~' in folder_path:
+        return os.path.expanduser(folder_path)
+    if os.path.isabs(folder_path):
+        return folder_path
+    else:
+        return os.path.abspath(folder_path)
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='openeobs_demo_data_generator',
+    version="0.1",
+    description="A set of tools to assist in the generation of demo data for"
+                "Open eObs",
+    author="NeovaHealth",
+    author_email="office@neovahealth.co.uk",
+    url="https://github.com/NeovaHealth/openeobs_demo_data_generator",
+    provides=["openeobs_demo_data_generator"],
+    packages=['demo_data_generators', 'demo_setup_tools', 'smoketest'],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "generate_openeobs_demo_data = demo_data_generators.__main__:main",
+        ]
+    },
+    install_requires=['ERPPeek==1.6.1', 'argparse>=1.2.1'],
+    license="GPL",
+    zip_safe=True,
+)

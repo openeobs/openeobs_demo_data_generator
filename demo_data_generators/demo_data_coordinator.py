1c678d9f04af57516635dbca150593997fced910
Lorenzo Pascucci <lorenzo@neovahealth.co.uk>
2016-01-08 10:42:30 +0000

    Improve demo data coordinator
    Refactor the XML files creation:
      - use the builtin write method from Python 'xml' library
      - accept a custom folder as parent folder for the files creation
    Also, integrate the users XML generator script.

--- demo_data_generators/demo_data_coordinator.py
+++ demo_data_generators/demo_data_coordinator.py
@@ -1,18 +1,21 @@
 # pylint: disable=R0903
 # pylint: disable=R0914
 """Coordinates demo data"""
-from xml.etree.ElementTree import tostring
-from demo_data_generators.patients import PatientsGenerator
-from demo_data_generators.spells import SpellsGenerator
-from demo_data_generators.admissions import AdmissionsGenerator
-from demo_data_generators.placements import PlacementsGenerator
 import re
 import os
+
+from xml.etree.ElementTree import ElementTree
+from demo_data_generators.admissions import AdmissionsGenerator
+from demo_data_generators.patients import PatientsGenerator
+from demo_data_generators.placements import PlacementsGenerator
+from demo_data_generators.spells import SpellsGenerator
+from demo_data_generators.users import UsersXMLGenerator
 
 
 class DemoDataCoordinator(object):
     """Coordinates demo data"""
-    def __init__(self, wards, bed_patient_per_ward, non_bed_patient_per_ward):
+    def __init__(self, pos, wards, beds_per_ward, bed_patient_per_ward,
+                 non_bed_patient_per_ward, users_schema, data_folder):
 
         total_patients_per_ward = \
             bed_patient_per_ward + non_bed_patient_per_ward
@@ -30,7 +33,16 @@ class DemoDataCoordinator(object):
         patient_id_regex_string = r'nhc_demo_patient_(\d+)'
         self.patient_id_regex = re.compile(patient_id_regex_string)
 
+        users_generator = UsersXMLGenerator(pos, users_schema)
+        users_generator.generate_multi_wards_users(wards)
+        users_generator.generate_users_not_assigned()
+        self.indent(users_generator.class_root)
+        users_tree = ElementTree(users_generator.class_root)
+        users_tree.write(os.path.join(data_folder, 'users.xml'))
+
         for index, ward in enumerate(wards):
+            users_per_ward_root = users_generator.generate_users_per_ward(
+                ward, beds_per_ward)
             patients = PatientsGenerator(
                 (index * total_patients_per_ward) + patient_id_offset,
                 bed_patient_per_ward,
@@ -41,23 +53,34 @@ class DemoDataCoordinator(object):
             admissions = AdmissionsGenerator(patients)
             placements = PlacementsGenerator(patients)
             # Pretty Print the XML file
+            self.indent(users_per_ward_root)
             self.indent(patients.root)
             self.indent(spells.root)
             self.indent(admissions.root)
             self.indent(placements.root)
 
-            if not os.path.isdir('ward_{0}'.format(ward)):
-                os.mkdir('ward_{0}'.format(ward))
-            with open('ward_{0}/demo_patients.xml'.format(ward), 'wb') as axml:
-                axml.write(tostring(patients.root))
-            with open('ward_{0}/demo_spells.xml'.format(ward), 'wb') as bxml:
-                bxml.write(tostring(spells.root))
-            with open('ward_{0}/demo_admissions.xml'.format(ward), 'wb') as \
-                    cxml:
-                cxml.write(tostring(admissions.root))
-            with open('ward_{0}/demo_placements.xml'.format(ward), 'wb') as \
-                    dxml:
-                dxml.write(tostring(placements.root))
+            ward_folder = os.path.join(data_folder, 'ward_{0}'.format(ward))
+
+            if not os.path.isdir(ward_folder):
+                os.mkdir(ward_folder)
+
+            users_per_ward_tree = ElementTree(users_per_ward_root)
+            users_per_ward_tree.write(os.path.join(ward_folder,
+                                                   'demo_users.xml'))
+
+            patients_tree = ElementTree(patients.root)
+            patients_tree.write(os.path.join(ward_folder, 'demo_patients.xml'))
+
+            spells_tree = ElementTree(spells.root)
+            spells_tree.write(os.path.join(ward_folder, 'demo_spells.xml'))
+
+            admissions_tree = ElementTree(admissions.root)
+            admissions_tree.write(os.path.join(ward_folder,
+                                               'demo_admissions.xml'))
+
+            placements_tree = ElementTree(placements.root)
+            placements_tree.write(os.path.join(ward_folder,
+                                               'demo_placements.xml'))
 
     def indent(self, elem, level=0):
         """Indents data"""

ab3f373b7beed00f9b8036a269aec16a1e72ec1d
Will Earp <will.earp@icloud.com>
2016-01-08 16:55:34 +0000

    broken execution obs down to 10 patients.

--- demo_setup_tools/observations.py
+++ demo_setup_tools/observations.py
@@ -7,7 +7,16 @@ class SubmitObservations(object):
         client = get_erppeek_client(server=server, db=database, user=user,
                                     password=password)
         SubmitInitialObservations(client, days)
-        SubmitFinalObservations(client, days)
+
+        patient_pool = client.model('nh.clinical.patient')
+        patient_ids = patient_pool([])
+
+        # break list of patient ids into sub-lists of length 10
+        n = 10
+        new_patient_ids = [patient_ids[i:i+n] for i in range(0, len(patient_ids), n)]
+        # submit observation 10 patients by 10 patients
+        for patients in new_patient_ids:
+            SubmitFinalObservations(client, days, patients)
 
 
 class SubmitInitialObservations(object):
@@ -23,10 +32,10 @@ class SubmitInitialObservations(object):
 
 class SubmitFinalObservations(object):
 
-    def __init__(self, client, days):
+    def __init__(self, client, days, patients):
         self.client = client
-        self.submit_observations(days)
+        self.submit_observations(days, patients)
 
-    def submit_observations(self, days):
+    def submit_observations(self, days, patients):
         api_demo = self.client.model('nh.eobs.demo.loader')
-        api_demo.generate_news_simulation(days)
+        api_demo.generate_news_simulation(days, patients)

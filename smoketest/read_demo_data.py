923a876cc1636248fe6ae6b4b61b36073be9808e
Lorenzo Pascucci <lorenzo@neovahealth.co.uk>
2016-01-08 17:25:22 +0000

    Remove exception handling
    Avoid masking the real exception and let it bubble up instead.
    Also, update the .gitignore file.

--- smoketest/read_demo_data.py
+++ smoketest/read_demo_data.py
@@ -13,15 +13,8 @@ def get_erppeek_client(server='http://localhost:8069', db='openerp',
     :param password: Password for username connecting with
     :return: A erppeek.Client object we can use for XML-RPC calls
     """
-    try:
-        client = erppeek.Client(server, db=db, user=user, password=password,
-                                verbose=False)
-    except:
-        raise RuntimeError(
-            "Error connecting to {0} on {1} using credentials {2}:{3}".format(
-                db, server, user, password
-            )
-        )
+    client = erppeek.Client(server, db=db, user=user, password=password,
+                            verbose=False)
     return client
 
 

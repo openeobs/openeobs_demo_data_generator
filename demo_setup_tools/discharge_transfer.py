0df182c21b3387c033d8f0668c0aeaf3dc26e0de
Will Earp <will.earp@icloud.com>
2016-01-13 17:36:22 +0000

    changed order of discharge and transfers in coordinator. changed names of discharge and transfer methods.

--- demo_setup_tools/discharge_transfer.py
+++ demo_setup_tools/discharge_transfer.py
@@ -18,7 +18,7 @@ def get_erppeek_client(server='http://localhost:8069', db='openerp',
         except:
             raise RuntimeError(
                 "Error connecting to {0} on {1} "
-                "using credentials {2}:{3}".format( db, server, user, password)
+                "using credentials {2}:{3}".format(db, server, user, password)
             )
         return client
 
@@ -29,24 +29,24 @@ class DischargeTransferCoordinator(object):
         client = get_erppeek_client(server=server, db=database, user=user,
                                     password=password)
 
-        TransferPatients(client, 'A', 'B', 4)
-        TransferPatients(client, 'B', 'C', 4)
-        TransferPatients(client, 'C', 'D', 4)
-        TransferPatients(client, 'D', 'E', 4)
-        TransferPatients(client, 'E', 'A', 4)
-
         DischargePatients(client, 'A', 4)
         DischargePatients(client, 'B', 4)
         DischargePatients(client, 'C', 4)
         DischargePatients(client, 'D', 4)
         DischargePatients(client, 'E', 4)
 
+        TransferPatients(client, 'A', 'B', 4)
+        TransferPatients(client, 'B', 'C', 4)
+        TransferPatients(client, 'C', 'D', 4)
+        TransferPatients(client, 'D', 'E', 4)
+        TransferPatients(client, 'E', 'A', 4)
+
 
 class DischargePatients(object):
 
     def __init__(self, client, discharge_ward, patients_to_affect):
         self.client = client
-        self.discharge_patient(discharge_ward, patients_to_affect)
+        self.discharge_patients(discharge_ward, patients_to_affect)
 
     def discharge_patient(self, ward, patients):
         api_demo = self.client.model('nh.eobs.demo.loader')
@@ -57,8 +57,8 @@ class TransferPatients(object):
 
     def __init__(self, client, from_ward, to_ward, patients):
         self.client = client
-        self.transfer_patient(from_ward, to_ward, patients)
+        self.transfer_patients(from_ward, to_ward, patients)
 
-    def transfer_patient(self, from_ward, to_ward, patients):
+    def transfer_patients(self, from_ward, to_ward, patients):
         api_demo = self.client.model('nh.eobs.demo.loader')
         api_demo.transfer_patients(from_ward, to_ward, patients)

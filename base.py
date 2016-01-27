8a0a6e31cb92a87a492fc95555b2d88ae4449102
Will Earp <will.earp@icloud.com>
2016-01-27 08:30:38 +0000

    added base classes Openerp, data, record, field.

---
+++ base.py
@@ -0,0 +1,27 @@
+from xml.etree.ElementTree import Element, SubElement
+
+
+class Openerp(object):
+    
+    def append_data(self):
+        return
+
+
+class Data(object):
+    
+    def append_record(self):
+        return
+
+
+class Record(object):
+    
+    def append_field(self):
+        return
+
+
+class Field(object):
+    pass
+
+
+
+   

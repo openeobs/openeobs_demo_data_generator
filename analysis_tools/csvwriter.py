6b220f13abfec9cc65c5d7c86967fb6170c7818f
Will Earp <will.earp@icloud.com>
2016-02-08 18:27:12 +0000

    [WI-2198] Added __init__.py and test methods.

--- analysis_tools/csvwriter.py
+++ analysis_tools/csvwriter.py
@@ -1,6 +1,16 @@
 import csv
 
 
-class UserCSVWriter(csv):
-    # [[id, name, [role1, role2]]...]
-    pass
+class UserCSVWriter(object):
+
+    fieldnames = ['user_id', 'username', 'System Administrator', 'Kiosk',
+                  'Senior Manager', 'Receptionist', 'Doctor', 'Senior Doctor',
+                  'Junior Doctor', 'Registrar', 'Consultant', 'Ward Manager',
+                  'Nurse', 'HCA']
+
+    def __init__(self, filename, users):
+        users.insert(0, self.fieldnames)
+        with open(filename, 'wb') as csv_output_file:
+            writer = csv.writer(csv_output_file)
+            writer.writerows(users)
+
--- analysis_tools/tests/test_csvwriter.py
+++ analysis_tools/tests/test_csvwriter.py
@@ -1,6 +1,30 @@
+import os
+import csv
 from unittest import TestCase
+
+from analysis_tools.csvwriter import UserCSVWriter
 
 
 class TestUserCSVWriter(TestCase):
 
-    pass
+    def setUp(self):
+        self.users = [
+            [1, 'nora', 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+            [2, 'waino', 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ]
+
+    def tearDown(self):
+        os.remove('test.csv')
+
+    def test_UserCSVWriter_writes_to_file(self):
+        UserCSVWriter('test.csv', self.users)
+        with open('test.csv', 'rb') as f:
+            reader = csv.DictReader(f)
+            row = reader.next()
+            self.assertEqual(row['user_id'], '1')
+            self.assertEqual(row['username'], 'nora')
+            self.assertEqual(row['System Administrator'], '1')
+            row = reader.next()
+            self.assertEqual(row['user_id'], '2')
+            self.assertEqual(row['username'], 'waino')
+            self.assertEqual(row['Kiosk'], '0')

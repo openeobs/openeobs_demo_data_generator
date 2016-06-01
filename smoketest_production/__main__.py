82a033f5d2ac2d237b2522d50c6491bab385a693
gregorlenz <gregorlenz@neovahealth.co.uk>
2016-06-01 14:10:24 +0100

    setup and teardown of database duplicate

--- smoketest_production/__main__.py
+++ smoketest_production/__main__.py
@@ -1,4 +1,32 @@
 import sys
 import unittest
 import argparse
-from smoke_test_production import
+from smoke_test_production import SmokeTestProduction
+
+PARSER = argparse.ArgumentParser('Production smoke tests')
+PARSER.add_argument('database', type=str,
+                    help='Database to run tests against',
+                    default='nhclinical')
+PARSER.add_argument('--server', type=str,
+                    help='Server to run tests against',
+                    default='http://54.171.180.53')
+PARSER.add_argument('--user', type=str,
+                    help='User to run tests as',
+                    default='admin')
+PARSER.add_argument('--password', type=str,
+                    help='Password for testing user', default='admin')
+
+
+def main():
+    args = PARSER.parse_args()
+    SmokeTestProduction.SERVER = args.server
+    SmokeTestProduction.DATABASE = args.database
+    SmokeTestProduction.USER = 'admin'
+    SmokeTestProduction.PASSWORD = 'admin'
+
+    suite = unittest.TestLoader().loadTestsFromTestCase(SmokeTestProduction)
+    unittest.TextTestRunner(verbosity=2).run(suite)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
--- smoketest_production/smoke_test_production.py
+++ smoketest_production/smoke_test_production.py
@@ -3,28 +3,46 @@ import unittest
 
 class SmokeTestProduction(unittest.TestCase):
 
-    SERVER = 'http://localhost:8069'
+    SERVER = 'http://54.171.180.53'
     DATABASE = 'nhclinical'
     TEST_DATABASE = 'nhclinical_duplicate'
     USER = 'admin'
     PASSWORD = 'admin'
 
+    @classmethod
+    def setUpClass(cls):
+        cls.client = erppeek.Client(server=cls.SERVER, db=cls.DATABASE, user=cls.USER, password=cls.PASSWORD)
+
+        if cls.client.db.db_exist(cls.TEST_DATABASE):
+            cls.client.db.drop(cls.USER, cls.TEST_DATABASE)
+
+        cls.client.db.duplicate_database(cls.USER, cls.DATABASE, cls.TEST_DATABASE)
+
     def setUp(self):
-        self.client = erppeek.Client(server=self.SERVER, db=self.DATABASE, user=self.USER, password=self.PASSWORD)
+        self.c = erppeek.Client(server=self.SERVER, db=self.TEST_DATABASE, user=self.USER, password=self.PASSWORD)
 
-        self.client.db.duplicate_database(self.USER, self.DATABASE, self.TEST_DATABASE)
-
-        self.client = self.client.login(self.USER, self.PASSWORD, self.TEST_DATABASE)
-
-        self.p = self.client.model('nh.clinical.patient')
+        self.p = self.c.model('nh.clinical.patient')
         self.a = self.client.model('nh.activity')
         self.s = self.client.model('nh.clinical.spell')
         self.l = self.client.model('nh.clinical.location')
         self.api = self.client.model('nh.eobs.api')
 
-    def test_patient_can_be_created(self):
+    @classmethod
+    def tearDownClass(cls):
+        cls.client.db.drop(cls.USER, cls.TEST_DATABASE)
 
-    def test_ward_can_be_created(self):
+    def test_port_8069_is_accessible(self):
+        pass
 
-    def test_user_can_be_created(self):
+    def test_port_5432_is_accessible(self):
+        pass
+
+    def test_example_patient_can_be_created(self):
+        pass
+
+    def test_example_ward_can_be_created(self):
+        pass
+
+    def test_example_user_can_be_created(self):
+        pass
 
